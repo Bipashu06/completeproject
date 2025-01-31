2f21f7cb7ed1b14ed982c5a196c2eba2e07afa6f
Bipashu06 <pk1467001@gmail.com>
2025-01-31 13:13:57 +0530

    change

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const db = require('./models');
+
 const Port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
