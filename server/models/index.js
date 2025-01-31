09b1f996cf6cdc40fe733d7716413d5b7768a892
Bipashu06 <pk1467001@gmail.com>
2025-01-31 12:22:17 +0530

    chages

--- server/models/index.js
+++ server/models/index.js
@@ -8,10 +8,16 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+require("dotenv").config();
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "mysql",
+    port: process.env.DB_PORT || 3306,
+    logging: false,
+  });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
